792ba710374f61140dbe8555e5082cf7c5585374
tharuka-pavith <tharukapavith2@gmail.com>
2023-03-14 22:11:04 +0530

    Add persistance storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,31 @@
  */
 
 import { configureStore } from '@reduxjs/toolkit';
-
 import loginReducer from './loginSlice';
 
-export default configureStore({
-    //store holds all our reducers and manages them for us.
-	reducer: {
-        login: loginReducer
-    },
-});
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+
+const persistedReducer = persistReducer(persistConfig, loginReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: [thunk]
+  });
+
+export const persistor = persistStore(store);
+
+
+// export default configureStore({
+//     //store holds all our reducers and manages them for us.
+// 	reducer: {
+//         login: loginReducer
+//     },
+// });
