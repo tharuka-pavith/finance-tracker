8cec7337e7daa196fcc7735f2dbcf33d7499ef2e
tharuka-pavith <tharukapavith2@gmail.com>
2023-03-17 13:26:48 +0530

    Add dashboard pages

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -6,25 +6,29 @@ import Welcome from '../pages/Welcome';
 import Signup from "../pages/Signup";
 import Login from "../pages/Login";
 import NotFound from "../pages/NotFound";
+import Dashboard from "../pages/Dashboard";
+import NewBook from "../pages/NewBook";
+import CashBook from "../pages/Cashbook";
+import CashIn from "../pages/CashIn";
+import CashOut from '../pages/CashOut';
 
 function AppRouter() {
     return (
         <Routes>
-            <Route path="/home/" element={<Home />} >
+            <Route path="/home" element={<Home />} >
                 <Route path="/home/" element={<Welcome />} />
                 <Route path="/home/login/" element={<Login />} />
                 <Route path="/home/signup/" element={<Signup />} />
                 <Route path="*" element={<NotFound />} />
             </Route>
 
-            {/* <Route path="/dashboard" element={<Dashboard />} >
-                <Route path="newtask" element={<NewTask />} />
-                <Route path="mytasks" element={<MyTask />} />
-                <Route path="todotasks" element={<TodoTask />} />
-                <Route path="history" element={<History />} />
-                <Route path="admin" element={<Admin />} />
+            <Route path="/dashboard" element={<Dashboard />} >
+                <Route path="/dashboard/newbook/" element={<NewBook />} />
+                <Route path="/dashboard/cashbook/" element={<CashBook />} />
+                <Route path="/dashboard/cash-in/" element={<CashIn />} />
+                <Route path="/dashboard/cash-out/" element={<CashOut />} />
                 <Route path="*" element={<NotFound />} />
-            </Route> */}
+            </Route>
 
 
             <Route path="*" element={<NotFound />} />
