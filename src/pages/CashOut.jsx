77f4257dcc25c7813bef4edc1cffc90240836f96
tharuka-pavith <tharukapavith2@gmail.com>
2023-03-17 18:48:48 +0530

    Connect http api endpoints

--- src/pages/CashOut.jsx
+++ src/pages/CashOut.jsx
@@ -16,6 +16,14 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { DesktopTimePicker } from '@mui/x-date-pickers/DesktopTimePicker';
 import dayjs from 'dayjs';
 
+//http requests
+import axios from 'axios';
+
+//redux store
+import { store } from '../redux/store';
+
+import { useNavigate } from 'react-router-dom';
+
 const paperStyles = {
     margin: '50px',
     borderRadius: '10px',
@@ -36,8 +44,36 @@ const buttonStyle = {
 }
 
 function CashOut() {
+    const bookId = store.getState().books.currentBook;
+    const navigate = useNavigate();
+
     const [entryDate, setEntryDate] = useState('');
     const [entryTime, setEntryTime] = useState('');
+    const [amount, setAmount] = useState(0.0);
+    const [remark, setremark] = useState('');
+
+    const today = dayjs();
+
+    const postData = () => {
+        const date = new Date(entryDate);
+        const time = new Date(entryTime);
+        const finalDate = new Date(date.getFullYear(), date.getMonth(), date.getDay(), time.getHours(), time.getMinutes());
+        console.log(finalDate);
+        axios.post(`http://localhost:3001/api/post/entry`, {
+                "book_id": bookId,
+                "date" : finalDate,
+                "amount": amount,
+                "remark": remark,
+                "type": false //Cash Out1
+            }).then((res) =>{
+                console.log(res.data);
+                navigate("/dashboard/cashbook");
+            }).catch((error)=>{
+                alert("Error saving data!");
+                console.log(error);
+            });
+    }
+
     return (
         <Container maxWidth='sm'>
             <Paper variant='outlined' sx={paperStyles}>
@@ -59,20 +95,24 @@ function CashOut() {
                             </LocalizationProvider>
                         </Grid>
                         <Grid item xs={12}>
-                        <TextField id="outlined-basic2" label="Amount" variant="outlined" />
+                        <TextField value={amount} required id="amount-id" label="Amount" variant="outlined" 
+                            onChange={(e) => setAmount(e.target.value)} />
                         </Grid>
                         <Grid item xs={12}>
-                        <TextField fullWidth id="outlined-basic2" label="Description" variant="outlined" />
+                        <TextField value={remark} fullWidth id="description-id" label="Description" variant="outlined" 
+                            onChange={(e) => setremark(e.target.value)} />
                         </Grid>
                         <Grid item xs={12}>
                         <Typography color='success' variant='subtitle2' textAlign={'left'} fontWeight="medium" sx={{ my: '10px' }}>Bill (Image): </Typography>
                         <Button variant='outlined'>Upload</Button>
                         </Grid>
                         <Grid item xs={6}>
-                            <Button href='/dashboard/cashbook' size={'large'} sx={buttonStyle} variant="outlined" color='error'>Cancel</Button>
+                        <Button size={'large'} sx={buttonStyle} variant="outlined" color='error'
+                                onClick={() => navigate("/dashboard/cashbook")}>Cancel</Button>
                         </Grid>
                         <Grid item xs={6}>
-                            <Button href='/dashboard/cashbook' size={'large'} sx={buttonStyle} variant="contained" color='success'>Save</Button>
+                        <Button size={'large'} sx={buttonStyle} variant="contained" color='success'
+                                onClick={postData}>Save</Button> {/*TODO: handle onClick => http POST */}
                         </Grid>
                     </Grid>
                 </Box>
