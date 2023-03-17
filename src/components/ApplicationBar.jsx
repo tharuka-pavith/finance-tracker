0aa0a98b8c723209d08ca172dcae859a01531e7c
tharuka-pavith <tharukapavith2@gmail.com>
2023-03-17 13:26:09 +0530

    Create user login functionality

--- src/components/ApplicationBar.jsx
+++ src/components/ApplicationBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -7,9 +7,39 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/system/Stack';
 import MenuIcon from '@mui/icons-material/Menu';
+import Tooltip from '@mui/material/Tooltip';
 
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { useDispatch } from 'react-redux';
+import { storeLogin } from '../redux/loginSlice';
+import { storeBooks } from '../redux/booksSlice';
+import { store } from '../redux/store';
 
 function ApplicationBar() {
+    const navigate = useNavigate();
+    //const username = useSelector(state => state.login.name);
+    const username = store.getState().login.name;
+    const dispatch = useDispatch();
+    console.log(`Application bar: ${username}`);
+
+    const logMeOut =() =>{
+        dispatch(
+            storeLogin({
+                name: "",
+                email: "",
+                password: "",
+                _id: "",
+                __v: 0
+            })
+        );
+        dispatch(
+            storeBooks({
+                books: []
+            })
+        )
+    }
+
     return (
         <Box sx={{ flexGrow: 1, display: 'flex', width: "100%", zIndex: 'tooltip', position: 'fixed', top: 0, marginX: 0}}>
             <AppBar position="static" sx={{ padding: "1rem", backgroundColor: "#3F72AF" }} elevation={1}>
@@ -30,7 +60,17 @@ function ApplicationBar() {
                         </Typography>
                     </Stack>
 
-                    <Button href='/home/login' color="inherit">Login</Button>
+                    <Tooltip title={ (username === "")? 'Login' : "Logout" }>
+                    <Button  color="inherit" 
+                    onClick={()=>{
+                        if (username ===  ""){
+                            navigate("/home/login")
+                        }else{ 
+                            navigate("/home/");
+                            logMeOut();}}}>
+                        { (username ===  "")? 'login' : username }
+                    </Button>
+                    </Tooltip>
                 </Toolbar>
             </AppBar>
         </Box>
